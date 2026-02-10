4e9bf795dd27dbe612aa83d71325a8776c35efee
Matt Dyson <matt@thedysons.net>
2026-02-10 17:08:24 +0000

    Add unique constraint for regions

--- apps/backend/src/migrations/011_add_indexes_constraints.ts
+++ apps/backend/src/migrations/011_add_indexes_constraints.ts
@@ -17,6 +17,7 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema.createIndex('idx_country_name').on('country').column('name').unique().execute();
   await db.schema.createIndex('idx_grape_name').on('grape').column('name').unique().execute();
   await db.schema.createIndex('idx_winegrape_wine_grape').on('winegrape').columns(['wineId', 'grapeId']).unique().execute();
+  await db.schema.createIndex('idx_region_name_countryId').on('region').columns(['name', 'countryId']).unique().execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
@@ -33,4 +34,5 @@ export async function down(db: Kysely<any>): Promise<void> {
   await db.schema.dropIndex('idx_country_name').execute();
   await db.schema.dropIndex('idx_grape_name').execute();
   await db.schema.dropIndex('idx_winegrape_wine_grape').execute();
+  await db.schema.dropIndex('idx_region_name_countryId').execute();
 }
