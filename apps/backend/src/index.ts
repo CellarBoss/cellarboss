c3414db43068db66a44a9c72644b2c54a2a5c080
Matt Dyson <matt@thedysons.net>
2026-03-11 17:35:55 +0000

    Use APP_VERSION

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -43,7 +43,7 @@ api.doc("/openapi.json", {
   openapi: "3.1.0",
   info: {
     title: "Cellarboss API",
-    version: "1.0.0",
+    version: process.env.APP_VERSION || "development",
     description: "Wine cellar management API",
   },
 });
