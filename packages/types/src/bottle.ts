3f81eccff08148043e35306baf8e81cb04329da9
Matt Dyson <matt@thedysons.net>
2026-03-14 18:40:48 +0000

    Add new type for bottle size

--- packages/types/src/bottle.ts
+++ packages/types/src/bottle.ts
@@ -5,6 +5,18 @@ export interface Bottle {
   vintageId: number;
   storageId: number | null;
   status: "ordered" | "stored" | "in-primeur" | "drunk" | "sold" | "gifted";
+  size:
+    | "piccolo"
+    | "half"
+    | "standard"
+    | "litre"
+    | "magnum"
+    | "double-magnum"
+    | "jeroboam"
+    | "imperial"
+    | "salmanazar"
+    | "balthazar"
+    | "nebuchadnezzar";
 }
 
 export type CreateBottle = Omit<Bottle, "id">;
