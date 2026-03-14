f5f4bd9759c0d40a9d02cdb119e82be89200ca20
Matt Dyson <matt@thedysons.net>
2026-03-14 18:19:38 +0000

    chore: Migrate from NextJS middleware to proxy (#131)
    ## Summary
    
    Run `npx @next/codemod@canary middleware-to-proxy .` on Web UI to
    migrate
    
    ## Changes
    
    - No changes necessary, only change of file name
    
    ## Type of Change
    
    - [ ] Bug fix
    - [ ] New feature
    - [ ] Refactor / code improvement
    - [x] Dependency update
    - [ ] Docs / config only
    
    ## Areas affected
    
    - [ ] Backend
    - [x] Web application
    - [ ] Project scaffolding (Docker images etc)
    
    ## Related Issues
    
    Fixes #47

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 const publicRoutes = ["/login", "/api/auth"];
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   if (publicRoutes.some((route) => pathname.startsWith(route))) {
