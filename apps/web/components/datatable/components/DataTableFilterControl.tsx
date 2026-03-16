1b0466191cfe6616465fb1f318c5ab9a1f4bf052
Matt Dyson <matt@thedysons.net>
2026-03-16 20:43:06 +0000

    Make DataTable and associated components more mobile friendly

--- apps/web/components/datatable/components/DataTableFilterControl.tsx
+++ apps/web/components/datatable/components/DataTableFilterControl.tsx
@@ -118,7 +118,7 @@ export function DataTableFilterControl<T>({
   };
 
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex flex-wrap items-center gap-2">
       {filters.map((filter) =>
         getFilterComponent(filter, table, columnFilters),
       )}
