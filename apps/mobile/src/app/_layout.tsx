ee17b77ba0fb761ee3fd4c32ed6266ef65c8e14a
Matt Dyson <matt@thedysons.net>
2026-03-30 20:09:33 +0100

    Register translation as required by date picker component

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -4,9 +4,13 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PaperProvider, ActivityIndicator } from "react-native-paper";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { en, registerTranslation } from "react-native-paper-dates";
 import { AuthProvider, useAuth } from "@/contexts/auth-context";
 import { NavigationHistoryProvider } from "@/contexts/navigation-history-context";
 import { theme } from "@/lib/theme";
+
+// TODO: Support multiple languages and read the user's preferred language from the server settings
+registerTranslation("en", en);
 
 const queryClient = new QueryClient();
 
