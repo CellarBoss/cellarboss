b76e090641157d904d3007d97c7e76519cf0e9d6
Matt Dyson <matt@thedysons.net>
2026-03-31 17:21:41 +0100

    Add aria-label to sidebar for use in tests

--- apps/web/components/sidebar/AppSidebar.tsx
+++ apps/web/components/sidebar/AppSidebar.tsx
@@ -157,6 +157,7 @@ export function AppSidebar() {
               <DropdownMenuTrigger asChild>
                 <SidebarMenuButton
                   size="lg"
+                  aria-label="User menu"
                   className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                 >
                   <Avatar className="h-8 w-8 rounded-lg">
