ed47224b4f6b50209c9474c6698c5b789646d0b1
Matt Dyson <matt@thedysons.net>
2026-04-14 11:20:28 +0100

    Fix for initial page not loading

--- apps/mobile/src/app/(app)/(tabs)/(cellar)/_layout.tsx
+++ apps/mobile/src/app/(app)/(tabs)/(cellar)/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
 
+export const unstable_settings = {
+  initialRouteName: "bottles/index",
+};
+
 export default function CellarStackLayout() {
   return <Stack screenOptions={{ headerShown: false }} />;
 }
