4c0568536a6a5c5e0b0c4a75e51e240cfb17c444
Matt Dyson <matt@thedysons.net>
2026-06-03 14:01:43 +0100

    Add backwards compatibility using compat. Downgrade some errors to warnings. See #544 for inspiration!

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,14 +1,30 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import { fixupConfigRules } from "@eslint/compat";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...fixupConfigRules(nextVitals),
+  ...fixupConfigRules(nextTs),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react-hooks/refs": "warn",
+      "react-hooks/set-state-in-effect": "warn",
+      "react-hooks/use-memo": "warn",
+    },
+  },
+  {
+    files: ["e2e/**/*.{ts,tsx}"],
+    rules: {
+      "react-hooks/rules-of-hooks": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    "**/.vitepress/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
